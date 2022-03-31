127acf347c132507f6635aed865ee2c45c4d129a
André Rodrigues <andre.rsilva96@hotmail.com>
2022-03-31 09:37:33 -0300

    seconds

--- resources/views/hour-bank.blade.php
+++ resources/views/hour-bank.blade.php
@@ -59,7 +59,7 @@
                         @php ($total += $val)
                     @endforeach
                     @if (count($tasks) == $j+1)
-                        <td>{{gmdate("d/m/Y H:i:s", $sec)}}</td>
+                        <td>{{gmdate("d/m/Y H:i:s", $sec)}} {{$val}}</td>
                         <td>{{Money::formatReal($total)}}</td>
                     @endif
                 @endforeach
